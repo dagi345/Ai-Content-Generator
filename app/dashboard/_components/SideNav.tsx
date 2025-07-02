9491eb03821b75c2cfad33c2d5042e386f497868
dagi345 <dagimabate456@gmail.com>
2025-07-02 07:13:01 -0800

    update

--- app/dashboard/_components/SideNav.tsx
+++ app/dashboard/_components/SideNav.tsx
@@ -1,13 +1,46 @@
+import {FileClock, HomeIcon, Settings, WalletCards} from "lucide-react"
 import Image from 'next/image'
 import React from 'react'
 
 const SideNav = () => {
+
+    const MenuList =[
+        {
+            name : "Home",
+            icon : HomeIcon , 
+            path : "/dashboard"
+        },
+         {
+            name : "History",
+            icon : FileClock , 
+            path : "/dashboard/history"
+        },
+         {
+            name : "Billing",
+            icon : WalletCards , 
+            path : "/dashboard/billing"
+        },
+         {
+            name : "Setting",
+            icon : Settings , 
+            path : "/dashboard/setting"
+        }
+    ]
+
   return (
     <div className='h-screen p-5 shadow-sm border'>
-        <div className="logo flex gap-2 items-center justify-center"> 
+        <div className="logo flex gap-2 items-center justify-center border-b-2 pb-2"> 
             <Image src={"/logo.svg"} alt='logo'  width={80} height={80} />
             <h1 className='font-serif font-bold'>Content Ai</h1>
+        </div>
+        <div className="">
+            {MenuList.map((menu , index)=>(
+                <div className="flex gap-2 mb-2 p-3">
+                    <menu.icon/>
+                    <h2>{menu.name}</h2>
+                </div>
 
+            ))}
         </div>
     </div>
   )
